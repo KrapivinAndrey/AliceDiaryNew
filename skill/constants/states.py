cc017d6d64934423a9ea18c0562f94fde7f995f2
a.krapivin <a.krapivin@kontur.ru>
2022-05-01 15:05:36 +0500

    feat: Перенес все дефолтные обработки

---
+++ skill/constants/states.py
@@ -0,0 +1,28 @@
+STATE_REQUEST_KEY = "session"
+STATE_RESPONSE_KEY = "session_state"
+USERSTATE_RESPONSE_KEY = "user_state_update"
+
+# Отладочная информация
+# Последние фразы пользователя
+PREVIOUS_MOVES = "prev_moves"
+
+# признак уточнения после того, как не смогли разобрать фразу.
+# Если снова не разобрали - выходим
+NEED_FALLBACK = "fallback"
+
+
+# region State of dialog
+
+STUDENTS = "students"
+
+# endregion
+
+# help menu
+PREVIOUS_STATE = "previous_state"
+NEXT_BUTTON = "next_button"
+
+# Эти состояния будут сохранены в fallback
+MUST_BE_SAVE = {PREVIOUS_STATE, NEXT_BUTTON}
+
+# Эти состояния сохраняются на каждый ход
+PERMANENT_VALUES = {STUDENTS}
---
+++ skill/scenes.py
@@ -0,0 +1,145 @@
+import inspect
+import sys
+
+from skill.alice import Request, button, image_button, image_list
+from skill.constants import entities, intents, states
+from skill.scenes_util import Scene
+
+# region Общие сцены
+
+# класс общая сцена
+
+
+class GlobalScene(Scene):
+    def reply(self, request: Request):
+        pass  # Здесь не нужно пока ничего делать
+
+    def handle_global_intents(self, request):
+
+        # Должны быть обработаны в первую очередь
+        if intents.HELP in request.intents:
+            return HelpMenu()
+        if intents.WHAT_CAN_YOU_DO in request.intents:
+            return WhatCanDo()
+        if intents.CLEAN in request.intents:
+            return ClearSettings()
+
+        # Глобальные команды
+        if intents.GET_SCHEDULE in request.intents:
+            pass
+        if intents.MAIN_MENU in request.intents:
+            pass
+
+    def handle_local_intents(self, request: Request):
+        pass  # Здесь не нужно пока ничего делать
+
+    def fallback(self, request: Request):
+        if request.session.get(states.NEED_FALLBACK, False):
+            text, tts = "Пытаюсь понять но не получается"
+            return self.make_response(request, text, tts, end_session=True)
+        else:
+            save_state = {}
+            # Сохраним важные состояние
+            for save in states.MUST_BE_SAVE:
+                if save in request.session:
+                    save_state.update({save: request.session[save]})
+            save_state[states.NEED_FALLBACK] = True
+            text, tts = "Не могу понять"
+            return self.make_response(
+                request,
+                text,
+                tts,
+                buttons=HELP,
+                state=save_state,
+            )
+
+
+class Welcome(GlobalScene):
+    def reply(self, request: Request):
+        text, tts = "Привет"
+        buttons = [
+            button("Что ты умеешь?"),
+        ]
+
+        return self.make_response(
+            request,
+            text,
+            tts,
+            buttons=buttons,
+            directives={"start_account_linking": {}},
+            user_state=None,
+        )
+
+    def handle_local_intents(self, request: Request):
+        pass
+
+
+class Goodbye(GlobalScene):
+    def reply(self, request: Request):
+        text, tts = "До свиданя"
+        return self.make_response(request, text, tts, end_session=True)
+
+
+class HaveMistake(GlobalScene):
+    def reply(self, request: Request):
+        text, tts = "Ошибка"
+        return self.make_response(request, text, tts, end_session=True)
+
+
+class WhatCanDo(GlobalScene):
+    def reply(self, request: Request):
+        return self.make_response(
+            request,
+            "Что я умею",
+            "Что я умею",
+            buttons=YES_NO,
+            state={},
+        )
+
+    def handle_local_intents(self, request: Request):
+        if intents.CONFIRM in request.intents:
+            return HelpMenu()
+        if intents.REJECT in request.intents:
+            return Welcome()
+
+
+class HelpMenu(GlobalScene):
+    def reply(self, request: Request):
+        return self.make_response(request, "Помощь", "Помощь", buttons=YES_NO)
+
+    def handle_local_intents(self, request: Request):
+        pass
+
+
+class ClearSettings(GlobalScene):
+    def reply(self, request: Request):
+
+        return self.make_response(
+            request,
+            "Сброс",
+            "Сброс",
+            state={},
+            user_state={x: None for x in request.user.keys()},
+            end_session=True,
+        )
+
+
+def _list_scenes():
+    current_module = sys.modules[__name__]
+    scenes = []
+    for name, obj in inspect.getmembers(current_module):
+        if inspect.isclass(obj) and issubclass(obj, Scene):
+            scenes.append(obj)
+    return scenes
+
+
+SCENES = {scene.id(): scene for scene in _list_scenes()}
+
+DEFAULT_SCENE = Welcome
+YES_NO = [button("Да"), button("Нет")]
+HELP = [button("Помощь")]
+DEFAULT_BUTTONS = [
+    button("Расписание на завтра"),
+    button("Главное меню"),
+]
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
