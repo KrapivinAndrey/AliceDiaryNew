afe93073bb7c8b14c9bbf3fc72a612c610d4d8d5
a.krapivin <a.krapivin@kontur.ru>
2022-05-04 22:38:23 +0500

    fix

--- skill/scenes_util.py
+++ skill/scenes_util.py
@@ -22,7 +22,7 @@ class Scene(ABC):
 
     def move(self, request: Request):
         if request.authorization_complete:
-            next_scene = self
+            next_scene = self.id()
         else:
             next_scene = self.handle_local_intents(request)
             if next_scene is None:
