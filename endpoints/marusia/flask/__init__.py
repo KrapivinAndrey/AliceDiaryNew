cdee1de9ca659f676ad2e68356bbbaa2d6aa3978
korotovskih <korotovskih@aol.com>
2022-08-04 22:02:52 +0500

    ендпоинт

---
+++ endpoints/marusia/flask/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from pydantic import ValidationError
+
+from .request_model import Model as RequestModel
+from .response_model import Model as ResponseModel
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_marusia():
+    return "<p>Hello, Marusia!</p>"
+
+
+@app.route("/marusia/202208", methods=["POST"])
+def marusia_202208():
+
+    try:
+        request_data = RequestModel.parse_raw(request.data)
+    except ValidationError as e:
+        print(e.json())
+        return e.json(), 400
+
+    response_data = ResponseModel(session=request_data.session)
+    response_data.response.text = "понг"
+
+    return response_data.json(), 200
---
+++ tests/fixtures/marusia/README.md
@@ -0,0 +1,3 @@
+# генерация моделей
+
+datamodel-codegen --input tests/fixtures/marusia/response-1.json --input-file-type json --output endpoints/marusia/flask/response_model.py
